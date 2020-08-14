fecff00370e00041f532304db61b507d66051a05
Matthew White <matthew-white@users.noreply.github.com>
2020-08-14 09:07:18 -0400

    Initial commit

---
+++ src/components/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div>
+      <button type="button" @click="toggleLocale">Toggle locale</button>
+    </div>
+    <div>Locale: <code>{{ $i18n.locale }}</code></div>
+    <hello-world/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './hello-world.vue';
+
+export default {
+  name: 'App',
+  components: { HelloWorld },
+  methods: {
+    toggleLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'en' ? 'fr' : 'en';
+    }
+  }
+};
+</script>
---
+++ src/components/hello-world.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ $t('hello') }}</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+};
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello World!"
+  },
+  "fr": {
+    "hello": "Salut<br>le Monde!"
+  }
+}
+</i18n>
